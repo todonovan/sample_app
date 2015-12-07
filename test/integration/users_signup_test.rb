309bf563236f261269c510e4cadfca69b85bc2f5
todonovan <terrence.odonovan@gmail.com>
2015-12-06 20:03:44 -0500

    initialize users update

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   
-  test "invalid signup informatio" do
+  test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, user: { name: "",
