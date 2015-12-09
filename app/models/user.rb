204d957fe01ef6d44442c830f94ec7ec5613a84d
todonovan <terrence.odonovan@gmail.com>
2015-12-08 22:00:20 -0500

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,21 +23,32 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
-  # Remembers a user in the database for use in persistent sessions
+  # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update_attribute(:remember_digest, nil)
   end
 
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  
   private
 
     def downcase_email
