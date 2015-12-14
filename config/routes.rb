15af115319d9c9e33c32ae095684e8f3cbb00f3f
todonovan <terrence.odonovan@gmail.com>
2015-12-13 23:19:09 -0500

    initialize relationships model

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,26 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  root 'static_pages#home'
   
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
+  root 'static_pages#home'
+
+  get                 'password_resets/new'
+  get                 'password_resets/edit'  
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts, only: [:create, :destroy]
-
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
-
+  resources :microposts,          only: [:create, :destroy]
+  resource  :relationships,       only: [:create, :destroy]
 end
